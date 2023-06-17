fe6862671d3261a8a0df38d21ebcceba9c1bf79b
Rohit kumar barada <rohitbarada@gmail.com>
2023-06-17 20:20:59 +0530

    "Adding our Serverless API"

---
+++ delete.js
@@ -0,0 +1,17 @@
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+  const params = {
+    TableName: process.env.tableName,
+    // 'Key' defines the partition key and sort key of the item to be removed
+    Key: {
+      userId: "123", // The id of the author
+      noteId: event.pathParameters.id, // The id of the note from the path
+    },
+  };
+
+  await dynamoDb.delete(params);
+
+  return { status: true };
+});
--- update.js
+++ update.js
@@ -3,7 +3,6 @@ import dynamoDb from "./libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
   const data = JSON.parse(event.body);
-
   const params = {
     TableName: process.env.tableName,
     // 'Key' defines the partition key and sort key of the item to be updated
@@ -14,7 +13,6 @@ export const main = handler(async (event, context) => {
     // 'UpdateExpression' defines the attributes to be updated
     // 'ExpressionAttributeValues' defines the value in the update expression
     UpdateExpression: "SET content = :content, attachment = :attachment",
-
     ExpressionAttributeValues: {
       ":attachment": data.attachment || null,
       ":content": data.content || null,
@@ -24,7 +22,8 @@ export const main = handler(async (event, context) => {
     // can inspect 'result' below to see how it works with different settings
     ReturnValues: "ALL_NEW",
   };
-  
+
   await dynamoDb.update(params);
+
   return { status: true };
 });
